c4ff008bde0b97fa8a7815c31517a7c8010b3e20
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-05-14 15:33:58 +0900

    💄 軽微修正

--- src/components/ui/ichigojam.tsx
+++ src/components/ui/ichigojam.tsx
@@ -3,16 +3,6 @@ import { cn } from "@/lib/utils";
 type IJTypographyProps = {
   children: React.ReactNode;
   color?: "default" | "primary" | "secondary";
-  fontWeight?:
-    | "black"
-    | "extrabold"
-    | "bold"
-    | "semibold"
-    | "medium"
-    | "normal"
-    | "light"
-    | "extralight"
-    | "thin";
   fontSize?:
     | "xs"
     | "sm"
@@ -32,18 +22,56 @@ type IJTypographyProps = {
 const IJTypography = ({
   children,
   color = "default",
-  fontWeight = "normal",
   fontSize = "base",
 }: IJTypographyProps) => {
+  const textSize = (() => {
+    switch (fontSize) {
+      case "xs":
+        return "text-xs";
+      case "sm":
+        return "text-sm";
+      case "base":
+        return "text-base";
+      case "lg":
+        return "text-lg";
+      case "xl":
+        return "text-xl";
+      case "2xl":
+        return "text-2xl";
+      case "3xl":
+        return "text-3xl";
+      case "4xl":
+        return "text-4xl";
+      case "5xl":
+        return "text-5xl";
+      case "6xl":
+        return "text-6xl";
+      case "7xl":
+        return "text-7xl";
+      case "8xl":
+        return "text-8xl";
+      case "9xl":
+        return "text-9xl";
+      default:
+        return "text-base";
+    }
+  })();
+
+  const textColor = (() => {
+    switch (color) {
+      case "default":
+        return "text-default";
+      case "primary":
+        return "text-primary";
+      case "secondary":
+        return "text-secondary";
+      default:
+        return "text-default";
+    }
+  })();
+
   return (
-    <div
-      className={cn(
-        `text-${color} font-${fontWeight} text-${fontSize}`,
-        "font-ichigojam",
-      )}
-    >
-      {children}
-    </div>
+    <div className={cn(textColor, textSize, "font-ichigojam")}>{children}</div>
   );
 };
 
