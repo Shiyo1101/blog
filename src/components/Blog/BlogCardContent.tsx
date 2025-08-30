96736f976a2c5343b089ace2a2c21e0b6bbf9d21
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-08-30 14:39:48 +0900

    💄 レスポンシブ対応

--- src/components/Blog/BlogCardContent.tsx
+++ src/components/Blog/BlogCardContent.tsx
@@ -12,17 +12,17 @@ const BlogCardContent = ({ post }: BlogCardContentProps) => {
   return (
     <motion.a
       href={`/blog/${post.slug}`}
-      className="block p-4 border-4 border-card-foreground bg-card shadow-[8px_8px_0_0] transition-shadow hover:shadow-none flex-col gap-4"
+      className="flex w-full flex-col gap-4 rounded-lg border-4 border-card-foreground bg-card p-4 shadow-[8px_8px_0_0] transition-shadow hover:shadow-none"
       whileHover={{ y: -5 }}
     >
-      <div className="flex h-48 items-center justify-center bg-background">
+      <div className="aspect-video overflow-hidden rounded-md bg-background">
         <img
           src={post.data.topImage}
           alt={post.data.title}
           className="h-full w-full object-cover"
         />
       </div>
-      <div className="p-4">
+      <div className="flex flex-col p-4">
         <h2 className="mb-2 text-xl font-bold">{post.data.title}</h2>
         <p className="text-foreground">{post.data.description}</p>
       </div>
