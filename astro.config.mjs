ba6945ee6644fa33c9366dbec5014d1b1608843c
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-09-26 14:25:40 +0900

    🔍 SEO強化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx({
+    optimize: true,
+  }), sitemap(), robotsTxt({
+    policy: [{ userAgent: "*", allow: "/" }],
+    sitemap: "https://uchii-blog.netlify.app/sitemap-index.xml",
+    host: "uchii-blog.netlify.app",
+  })],
   site: "https://uchii-blog.netlify.app",
 });
