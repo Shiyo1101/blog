bdc5dc314a6873450eb45c43c47071584693afac
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-09-26 15:46:57 +0900

    ♻️ フォーマット

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,22 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
 
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), mdx({
-    optimize: true,
-  }), sitemap(), robotsTxt({
-    policy: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://uchii-blog.netlify.app/sitemap-index.xml",
-    host: "uchii-blog.netlify.app",
-  })],
+  integrations: [
+    react(),
+    mdx({
+      optimize: true,
+    }),
+    sitemap(),
+    robotsTxt({
+      policy: [{ userAgent: "*", allow: "/" }],
+      sitemap: "https://uchii-blog.netlify.app/sitemap-index.xml",
+      host: "uchii-blog.netlify.app",
+    }),
+  ],
   site: "https://uchii-blog.netlify.app",
 });
